257f5774411d2023478a117625eb156e888acde0
gabrielanevesd <167656155+gabrielanevesd@users.noreply.github.com>
2025-11-26 19:50:58 -0300

    Update EstruturasCondicionais.java

--- EstruturasCondicionais.java
+++ EstruturasCondicionais.java
@@ -1,22 +1,9 @@
-//Escreva um programa que solicite ao usuário dois números inteiros.
-
-//Depois, verifique qual é o maior número.
-
-//Imprima no console o maior número, sendo o formato da mensagem o seguinte: "O maior número é: {numero}.". Exemplo: O maior número é: 7.
-
-//Caso os números sejam iguais, imprimir a mensagem: "Os números são iguais!"
-
-//Observação:
-//Utilizar o scanner para a entrada de dados dos dois números.
-//Para as condições, utilizar: if, else if, else.
-//Compartilhe o link do repositório na plataforma para a correção dos tutores.
-
 import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class EstruturasCondicionais {
     public static void main(String[] args) {
-        //Solicitar ao usuário dois números int: vou usar scanner e int
+        
         Scanner scanner = new Scanner(System.in);
 
         System.out.println("Digite um número: ");
@@ -26,8 +13,6 @@ public class EstruturasCondicionais {
         System.out.println("Digite outro número: ");
 
         int segundoNumero = scanner.nextInt();
-
-        //Qual número é maior?
 
         if (primeiroNumero > segundoNumero) {
 
